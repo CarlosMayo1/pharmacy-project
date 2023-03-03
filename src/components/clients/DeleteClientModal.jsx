db9ac1a517f798047cdd600d70d12c49691d74b5
carlosmayo <carloshmayo@gmail.com>
2023-03-03 00:22:25 -0500

    Delete modal almost done, it needs the functionality

--- src/components/clients/DeleteClientModal.jsx
+++ src/components/clients/DeleteClientModal.jsx
@@ -1,9 +1,19 @@
 import Modal from '../../UI/Modal'
 
+import classes from './DeleteClientModal.module.css'
+
 const DeleteClientModal = ({ onClose }) => {
   return (
     <Modal onClose={onClose}>
-      Hell this is working
+      <div className={classes.header}><i className='fa fa-triangle-exclamation' /></div>
+      <h2 className={classes.title}>Eliminar Cliente</h2>
+      <div className={classes.body}>
+        <p>¿Está seguro de eliminar el usuario?</p>
+      </div>
+      <div className={classes.actions}>
+        <button onClick={onClose}>Cancelar</button>
+        <button>Aceptar</button>
+      </div>
     </Modal>
   )
 }
